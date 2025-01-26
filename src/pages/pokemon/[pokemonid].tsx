95b32db4b430ba51f4a36e401359355f9f91da6a
Roberto de Oliveira <92410083+Popotomimi@users.noreply.github.com>
2025-01-26 19:27:20 -0300

    update types pokemons

--- src/pages/pokemon/[pokemonid].tsx
+++ src/pages/pokemon/[pokemonid].tsx
@@ -77,12 +77,48 @@ const PokemonId: React.FC<PokemonIdProps> = ({ pokemon }) => {
     );
   }, [pokemon.id]);
 
+  type PokemonTypeName =
+    | "grass"
+    | "poison"
+    | "fire"
+    | "water"
+    | "bug"
+    | "flying"
+    | "normal"
+    | "electric"
+    | "ground"
+    | "ghost"
+    | "fairy"
+    | "fighting"
+    | "psychic"
+    | "rock"
+    | "steel"
+    | "ice";
+
+  const typeColors: Record<PokemonTypeName, string> = {
+    grass: "bg-grass",
+    poison: "bg-poison",
+    fire: "bg-fire",
+    water: "bg-water",
+    bug: "bg-bug",
+    flying: "bg-flying",
+    normal: "bg-normal",
+    electric: "bg-electric",
+    ground: "bg-ground",
+    ghost: "bg-ghost",
+    fairy: "bg-fairy",
+    fighting: "bg-fighting",
+    psychic: "bg-psychic",
+    rock: "bg-rock",
+    steel: "bg-steel",
+    ice: "bg-ice",
+  };
+
   return (
     <div className="text-center">
       <h1 className="capitalize rounded-lg w-2/4 mx-auto mt-10 p-2 text-white text-3xl font-bold bg-zinc-800">
         {pokemon.name}
       </h1>
-
       <Image
         className="m-auto"
         src={imageSrc}
@@ -99,7 +135,9 @@ const PokemonId: React.FC<PokemonIdProps> = ({ pokemon }) => {
         <div>
           {pokemon.types.map((item, index) => (
             <span
-              className="bg-zinc-700 text-white uppercase font-bold m-1 p-1.5 pb-2.5 rounded-lg"
+              className={`${
+                typeColors[item.type.name as PokemonTypeName] || "bg-default"
+              } text-white uppercase font-bold m-1 p-1.5 pb-2.5 rounded-lg`}
               key={index}>
               {item.type.name}
             </span>
@@ -110,7 +148,7 @@ const PokemonId: React.FC<PokemonIdProps> = ({ pokemon }) => {
         <h3 className="font-bold mt-3">Altura:</h3>
         <p>{pokemon.height * 10} cm</p>
       </div>
-      <div>
+      <div className="mb-32">
         <h3 className="font-bold">Peso:</h3>
         <p>{pokemon.weight / 10} kg</p>
       </div>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,8 +11,25 @@ export default {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        grass: '#78C850',
+        poison: '#A040A0',
+        fire: '#F08030',
+        water: '#6890F0',
+        bug: '#A8B820',
+        flying: '#A890F0',
+        normal: '#A8A878',
+        electric: '#F8D030',
+        ground: '#E0C068',
+        ghost: '#d3d3d3',
+        fairy: '#FFB6C1',
+        fighting: '#D2B48C',
+        psychic: '#FFd700',
+        rock: '#d3d3d3',
+        steel: '#00008b',
+        ice: '#d3d3d3'
       },
     },
   },
   plugins: [],
 } satisfies Config;
+
